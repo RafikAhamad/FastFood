1ea27a850d04d96b8c4fafcf66610ef1ac18b770
Rafik <ra4ahmad@gmail.com>
2026-09-10 11:27:41 +0530

    Updated the header section

---
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./ExploreMenu.css"
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className="explore-menu" id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">Explore our delicious food menu, packed with fresh ingredients and flavorful dishes made to satisfy every craving. From juicy burgers and crispy sides to refreshing drinks and tasty desserts, there’s something for everyone. Choose your favorite meal, place your order, and enjoy great food delivered fresh to you!</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item, index) => {
+                return (
+                    <div key={index} className='explore-menu-list-item'>
+                        <img src={item.menu_image} alt="" />
+                        <p>{item.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Explore our delicious food menu, packed with fresh ingredients and flavorful dishes made to satisfy every craving. From juicy burgers and crispy sides to refreshing drinks and tasty desserts, there’s something for everyone. Choose your favorite meal, place your order, and enjoy great food delivered fresh to you!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
